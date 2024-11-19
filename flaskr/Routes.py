28eec994c5a5dc729ebf6070ccbc91bee1806946
GameWame <ninofrassica55@gmail.com>
2024-11-19 14:13:56 +0100

    webpage reduction
    set the base for future developments

--- flaskr/Routes.py
+++ flaskr/Routes.py
@@ -1,10 +1,13 @@
 from flask import render_template, Blueprint, session
 
-
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
 @views.route('/home')
 def home():
-    return render_template('home.html')
+    # sostituire variabili al posto degli 0
+    percarrivo = 50
+    numpass = 0
+    numtick = 0
+    return render_template('home.html', percentualeArrivo=percarrivo, numpasseggeri=numpass, numticket=numtick)
