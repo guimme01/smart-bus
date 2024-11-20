66dac51cb7aaef6be0e9ec8d44ab9f77a789afdf
GameWame <ninofrassica55@gmail.com>
2024-11-20 11:01:21 +0100

    Connection Test
    Connected with wifi and arduino module to handle POST request

--- flaskr/Routes.py
+++ flaskr/Routes.py
@@ -1,13 +1,24 @@
-from flask import render_template, Blueprint, session
+from flask import render_template, Blueprint, request
 
 views = Blueprint('views', __name__)
+
+numpass = 0
+data = 0
 
 
 @views.route('/')
 @views.route('/home')
 def home():
-    # sostituire variabili al posto degli 0
     percarrivo = 50
-    numpass = 0
     numtick = 0
     return render_template('home.html', percentualeArrivo=percarrivo, numpasseggeri=numpass, numticket=numtick)
+
+
+@views.route("/laserAction", methods=["GET", "POST"])
+def handle_post():
+    global numpass, data
+    if request.is_json:
+        data = request.json
+        numpass += 1
+
+    return render_template('home.html', numpasseggeri=numpass)
